22cbaf76f3ea2fbab0d6ee118bf986f4a549515d
bueinat <62245924+bueinat@users.noreply.github.com>
2021-05-04 01:13:47 +0300

    objects + requests

--- REST_API/api.js
+++ REST_API/api.js
@@ -9,14 +9,16 @@ const models = [
     { id: 3, name: "model3"},
 ]
 
+const port = 9876;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
 // main window
 app.get('/', (req, res) => {
-    // res.send('Hello World');
-    res.send(require('path').join(__dirname, "../temporary_files/simple_csharp_project/ConsoleApp1/ConsoleApp1/Program.cs"));
+    res.send('Welcome');
 });
 
 app.post('/api/model', (req, res) => {
-    // shape of object    
+    // shape of object
     const schema = Joi.object({
         name: Joi.string().min(3).required()
     });
@@ -38,11 +40,81 @@ app.post('/api/model', (req, res) => {
 });
 
 app.get('/api/model', (req, res) => {
-    // let id = req.query.id;
+    // first find the given model
     let model = models.find(c => c.id === parseInt(req.query.id));
     if (!model) return res.status(404).send("the model was not found");
+
+    // return it if found
     res.send(model);
 });
 
-const port = 9876;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// TODO: write a second request for each confusing one, such as here
+app.get('/api/model/:id', (req, res) => {
+    // first find the given model
+    let model = models.find(c => c.id === parseInt(req.params.id));
+    if (!model) return res.status(404).send("the model was not found");
+
+    // return it if found
+    res.send(model);
+});
+
+
+app.delete('/api/model', (req, res) => {
+    // first find the given model
+    let model = models.find(c => c.id === parseInt(req.params.id));
+    if (!model) return res.status(404).send("the model was not found");
+
+    // delete it if found
+    const index = models.indexOf(model);
+    models.splice(index, 1);
+    res.send(model);
+});
+
+app.get('/api/models', (req, res) => {
+    // res.send(models);
+    res.json({ models: models })
+});
+
+/* app.post('/api/model', (req, res) => {
+    // its body would be copied (or very similar to) model post
+    // TODO: use redirection somehow. May be useful also some places else
+}); */
+
+/*** OBJECTS DEFINITIONS ***/
+// By convention, constructor function
+// start with Uppercase letter.
+/**
+ * TODO:
+ * 1) write also data
+ * 2) throw errors
+ */
+
+class Model {
+    constructor(id, upload_time) {
+        this.id = id;
+        this.upload_time = upload_time;
+        this.status = "pending";
+    }
+    // some extra function may be needed
+}
+
+class Anomaly {
+    constructor(anomalies, reason) {
+        this.anomalies = anomalies;
+        this.reason = reason;
+    }
+    // some extra function may be needed
+}
+
+class Span {
+    constructor(start_time, end_time) {
+        this.start_time = start_time;
+        this.end_time = end_time;
+    }
+    // some extra function may be needed
+}
+    // When used with new, assigns this = {}
+    // and returns this at the end.
+    // {“name”: “Jack”, “isAdmin”: false}
+    // example: let Model = new Model("Jack");
+
